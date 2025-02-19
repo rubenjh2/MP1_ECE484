54148e4611e275bccf11a03f9824898968a07e35
Ruben Hernandez <rubenjh2@eceb-5072-15.ews.illinois.edu>
2025-02-18 20:48:52 -0600

    work

--- src/mp1/test_lane_detection.py
+++ src/mp1/test_lane_detection.py
@@ -54,7 +54,7 @@ def perspective_transform(image):
 
 
 # Function to visualize lane predictions for multiple images in a single row
-def visualize_lanes_row(images, instances_maps, alpha=0.7):
+def visualize_lanes_row(images, instances_maps, alpha=0.5):
     """
     Visualize lane predictions for multiple images in a single row
     For each image:
@@ -64,34 +64,61 @@ def visualize_lanes_row(images, instances_maps, alpha=0.7):
     """
     
     num_images = len(images)
-    fig, axes = plt.subplots(1, num_images, figsize=(15, 5))
+    fig, axes = plt.subplots(2, num_images, figsize=(15, 5))
 
     ####################### TODO: Your code starts Here #######################
+
+    axes = axes.flatten()
     
     for i in range(num_images):
         
         # resize the image to 512 x 256
         image_resized = cv2.resize(images[i], (512, 256))
+        # instance_map_resized = cv2.resize(instances_maps[i], (512, 256))
+        # instance_map_resized = instances_maps[i].astype(float) / 255.0
 
+        # # apply perspective transform to both the original image and its instance map
+        # image_transformed = perspective_transform(image_resized)
+        # instance_map_transformed = perspective_transform(instances_maps[i])
+
+        axes[i].imshow(image_resized)
+        axes[i].imshow(instances_maps[i], alpha=alpha)
+
+
+    for i in range(num_images):
 
         # apply perspective transform to both the original image and its instance map
         image_transformed = perspective_transform(image_resized)
-        instance_map_transformed_uint8 = perspective_transform(instances_maps[i])
+        instance_map_transformed = perspective_transform(instances_maps[i])
 
-        # to change instance_map_transformed to uint8 from float32 to match types
-        instance_map_transformed = (instance_map_transformed_uint8 * 255).astype(np.uint8)
-
-        print(type(image_transformed), image_transformed.dtype)
-
-        print(type(instance_map_transformed), instance_map_transformed.dtype)
+        axes[num_images+i].imshow(image_transformed)
+        axes[num_images+i].imshow(instance_map_transformed, alpha=alpha)
 
 
-        # overlay the instance map to a plot with the corresponding original image using a specified alpha value
-        overlay = cv2.addWeighted(instance_map_transformed, 1 - alpha, image_transformed, alpha, 0) # alpha is transparency factor
-
-
-        axes[i].imshow(overlay)
         axes[i].axis('off')
+
+
+        # image_transformed = perspective_transform(image_resized)
+        # instance_map_transformed_uint8 = perspective_transform(instance_map_resized)
+
+        # # to change instance_map_transformed to uint8 from float32 to match types
+        # instance_map_transformed = (instance_map_transformed_uint8 * 255).astype(np.uint8)
+
+        # # overlay the instance map to a plot with the corresponding original image using a specified alpha value
+
+        # instance_map_transformed = cv2.cvtColor(instance_map_transformed, cv2.COLOR_GRAY2BGR)
+
+
+        # print(type(image_transformed), image_transformed.shape)
+
+        # print(type(instance_map_transformed), instance_map_transformed.shape)
+
+
+        # overlay = cv2.addWeighted(image_transformed, alpha, instance_map_transformed, 1 - alpha, 0) # alpha is transparency factor
+
+
+        # axes[i].imshow(overlay)
+        # axes[i].axis('off')
 
 
 
